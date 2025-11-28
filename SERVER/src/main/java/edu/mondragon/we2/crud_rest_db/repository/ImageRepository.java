be5c4d63451b184079d9a08c2b289e1c68c1227a
eneritz19 <eneritz.grana@alumni.mondragon.edu>
2025-11-28 08:18:08 +0100

    docker

--- SERVER/src/main/java/edu/mondragon/we2/crud_rest_db/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mondragon.we2.crud_rest_db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.mondragon.we2.crud_rest_db.entity.Image;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-
-}
--- SERVER/src/main/java/edu/mondragon/we2/crud_rest_db/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mondragon.we2.crud_rest_db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.mondragon.we2.crud_rest_db.entity.Request;
-
-public interface RequestRepository extends JpaRepository<Request, Integer> {
-
-}
